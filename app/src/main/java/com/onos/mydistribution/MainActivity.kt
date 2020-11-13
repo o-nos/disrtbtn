e65273e825f79823591f5da4039588f73722b42c
o-nos <spacegatherer@gmail.com>
2020-11-13 13:29:14 +0200

    init commit

---
+++ app/src/main/java/com/onos/mydistribution/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.onos.mydistribution
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
